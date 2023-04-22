ec770a54e16cb3dc557354d375f3b7a3f90ef97d
Rishijay Shrivastava <rishijay.shrivastava@gmail.com>
2023-04-22 22:14:11 +0530

    v1.0.3 - Added chalk

--- postinstall.js
+++ postinstall.js
@@ -1,8 +1,19 @@
-const package = require('./package.json')
+import chalk from 'chalk';
 
-console.log('End of installation');
+const log = console.log;
 
-//Example using properties from package.json
-if (package.version.includes('alpha')) {
-  console.log('Warning: Alpha version!')
-}
+log("")
+log("")
+log("====================================================");
+log(chalk.greenBright(` Prettier config - v${process.env.npm_package_version} installed successfully.!!`));
+log("====================================================");
+log("")
+log(chalk.cyanBright("But it's not done yet.!!"))
+log("")
+log(chalk.cyanBright('Please add ' + chalk.yellowBright.bold('"prettier": "@rishijay/prettier-config"') + ' in the main scope of package.json to complete the installation.'));
+log("")
+log(chalk.yellow.underline("Example :-"))
+log("")
+log(chalk.yellowBright(`{\n  "name": "your-project-name",\n  "version": "x.x.x",\n  `)+ chalk.yellow.underline(`"prettier": "@rishijay/prettier-config"`)+chalk.green.bold(" -> Add this line here")+chalk.yellowBright("\n  // ... \n}"))
+log("")
+log("")
